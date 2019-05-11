01fad15826c45ac6d1d8e38c334b1aed959e6b6f
Ben <48459562+bwilson1990@users.noreply.github.com>
2019-05-11 16:15:57 -0500

    added files
    completed working version

---
+++ README.md
@@ -0,0 +1,11 @@
+# unit-6-gifs (Giphy API)
+
+Created a website that is able to generate a button based on the text input 
+and search through the .gif API library from giphy.com.
+
+The specific tag for .gif results is "animal" to go along with my topic.
+
+When the button is clicked, the page generates 10 image results from the Giphy API.
+
+Also, this assignment shows how to use AJAX, a jQuery method to get the website url and API information
+to be generated in the DOM.
---
+++ script.js
@@ -0,0 +1,57 @@
+var animalButtons = ["chinchilla", "hamster", "bunny", "mouse", "rat"];
+var newButton;
+var animalImage;
+
+// button generator
+function generateButton(title) {
+    newButton = $('<button class="animal-button" data-animal="' + title + '">').text(title);
+    $("#button-holder").append(newButton);
+};
+
+for (var i = 0; i < animalButtons.length; i++) {
+    generateButton(animalButtons[i]);
+};
+
+
+// listening for clicks on add-button id
+$("#add-button").on("click", function() {
+    var title = $("#button-text").val();
+    generateButton(title);
+});
+
+// This .on("click") function will trigger the AJAX Call
+$("#button-holder").on("click", ".animal-button", function(event) {
+    $(".gifs-appear-here").html("");
+    var animal = $(this).attr("data-animal");
+    
+    // API URL to Giphy.com with API Key
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=tOotoZZfy7LUygoZTqmja2tWOg5Q1qlu&limit=10";
+
+    //AJAX
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+    console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var animalDiv = $('<div class="animal-gif-class">');
+            // animalDiv = $("<hr>")
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            animalImage = $("<img>");
+
+                animalImage.attr("src", results[i].images.fixed_height.url);
+                animalImage.attr("alt", results[i].title);
+
+                animalDiv.append(p);
+                animalDiv.append(animalImage);
+            
+            $("#gifs-appear-here").prepend(animalDiv);
+            
+        };
+
+    }); // end of AJAX function
+
+}); // end of button-holder function
